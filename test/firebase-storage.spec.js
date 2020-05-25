6536e81ec9a0e4de4bd14046ef1569471d180d7b
paula113 <paulapierina96@gmail.com>
2020-05-25 09:32:42 -0500

    Agregando objetivos de aprendizaje

--- test/firebase-storage.spec.js
+++ test/firebase-storage.spec.js
@@ -9,15 +9,6 @@ global.firebase = firebasemock.MockFirebaseSdk(
   () => null,
   () => mockauth,
 );
-// jest.mock('../src/firestore-controller/firebase-storage.js', () => {
-//   const mockfirestore = new firebasemock.MockFirestore();
-//   return new firebasemock.MockFirebaseSdk(
-//     null, // RTDB
-//     () => mockauth,
-//     () => mockfirestore,
-//   );
-// });
-
 
 // eslint-disable-next-line import/first
 import { uploadImagePost } from '../src/firestore-controller/firebase-storage.js';
@@ -31,55 +22,17 @@ const img = {
   uid: '38cj4',
 };
 
-// const user = {
-//   file: 'user.png',
-//   uid: '38cj4',
-// };
-// console.log(uploadImagePost(img.file, img.uid));
-// console.log();
-
 
 it('Deberia poder subir una img al storage', (done) => {
   const response = uploadImagePost(img.file, img.uid);
-  console.log(response.fullPath);
+  // console.log(response.fullPath);
 
   try {
     // console.log(response);
     expect(response.fullPath).toEqual(`imgPost/${img.uid}/${img.file.name}`);
     done();
   } catch (error) {
-    // done(error);
+    done(error);
     console.log(error.message);
   }
-  // test('the data is peanut butter', done => {
-  // function callback(data) {
-  //   try {
-  //     expect(data).toBe('peanut butter');
-  //     done();
-  //   } catch (error) {
-  //     done(error);
-  //   }
-  // }
-
-  // fetchData(callback);
 });
-
-// .then(() => {
-//   // console.log(response);
-//   // const result = data.find(note => note.img === `imgPost/${img.uid}/${img.file}`);
-//   expect('imgPost/38cj4/hola.png').toEqual(`imgPost/${img.uid}/${img.file}`);
-//   done();
-// })
-// .catch((error) => {
-//   // The document probably doesn't exist.
-//   console.error('Error updating document: ', error);
-// }));
-
-// it('Deberia poder subir una img al storage', done => uploadImagePost(img.file, img.uid)
-//   .then(() => getStatus(
-//     (data) => {
-//       const result = data.find(note => note.img === `imgPost/${img.uid}/${img.file}`);
-//       expect(result.file.name).toEqual();
-//       done();
-//     },
-//   )));
